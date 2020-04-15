6865c4734cb4cd5204abacf0b3ab4d9d9154f28b
EricNothing <814583328@qq.com>
2020-04-15 17:23:49 +0800

    修改配置文件

--- config/admin.php
+++ config/admin.php
@@ -11,7 +11,7 @@ return [
     | login page.
     |
     */
-    'name' => 'Laravel-admin',
+    'name' => 'SoulFire',
 
     /*
     |--------------------------------------------------------------------------
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>SF</b>',
 
     /*
     |--------------------------------------------------------------------------
